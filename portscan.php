8cc1a516998160df3dfc6d4c65afcb319683997a
Pablo Santhus <santhuspablo@gmail.com>
2017-01-29 14:06:35 -0200

    Update portscan.php

--- portscan.php
+++ portscan.php
@@ -7,6 +7,8 @@
    https://www.youtube.com/0xsec
 */
 
+
+<?php
 	error_reporting(0);
 
 
@@ -34,7 +36,7 @@ ______          _     _____
                            Criado by Pablo Santhus
 
   [+] Autor: Pablo Santhus
-  [+] Github: 
+  [+] Github: https://github.com/PabloSanthus
   [+] Help: $argv[0] -h
 \n\n";
 }
@@ -73,6 +75,7 @@ Porta                      Host               Servico
 				}
 			}
 		}
+
 		sleep(2);
 		echo "\n\n[$hora] Portas Encontradas . . .\n";
 		$file = file_get_contents("portscan.txt");
@@ -93,7 +96,7 @@ if($argv[1] == "-p" or $argv[2] == "-p"){
 	echo "[$hora] Host {$ip} \n";
 	sleep(2);
 	echo "[$hora] Iniciando Verificacao ...\n\n";
-	$ports = array(21, 22, 23, 25, 53, 63, 66, 67, 69, 80, 110, 123, 135, 139, 143, 156, 161, 179, 554, 902, 912, 2869, 5357, 389, 443, 445, 587, 1433, 1723, 1863, 3128, 3306,3389);
+	$ports = array(21, 22, 23, 25, 53, 63, 66, 67, 69, 80, 106, 110, 123, 135, 139, 143, 156, 161, 179, 465, 554, 902, 912, 2869, 5357, 389, 443, 445, 587, 1433, 1723, 1863, 3128, 3306,3389, 4444);
 	$result_port = array();
 	foreach($ports as $port){
 		if($p = fsockopen($ip, $port, $errnum, $errstr, 1)){
@@ -132,6 +135,77 @@ PORTA                       HOST                  SERVICO
 		fclose($fp);
 
 }
+
+if($argv[1] == "-g"){
+	date_default_timezone_set("America/Sao_Paulo");
+
+	$hora = date("h:i:s", time());
+	echo "[$hora] Scan todos os Hosts \n";
+	sleep(2);
+	echo "[$hora] Iniciando Verificacao ...\n\n";
+
+	$shell = shell_exec("arp -a");
+
+	preg_match_all('/(192)\.(168)\.\d{1,3}\.\d{1,3}/', $shell, $matches);
+	
+
+	for($i = 0; $i <= sizeof($matches[0]); $i ++){
+
+		$zp = fopen("ips_rede.txt", "a");
+		fwrite($zp, $matches[0][$i] . "
+");
+	}
+	$ipz = file("ips_rede.txt");
+	$ips = str_replace(" ", "", $ipz);
+	foreach($ips as $ip){
+
+		$ports = array(21, 22, 23, 25, 53, 63, 66, 67, 69, 80, 106, 110, 123, 135, 139, 143, 156, 161, 179, 465, 554, 902, 912, 2869, 5357, 389, 443, 445, 587, 1433, 1723, 1863, 3128, 3306, 3389);
+		$results = array();
+
+	foreach($ports as $port){
+
+		 if($pf = @fsockopen("", $port, $err, $err_string, 1)) {
+            $results[$port] = true;
+            fclose($pf);
+        } else {
+            $results[$port] = false;
+        }
+	}
+
+	$fp = fopen("portscan.txt", "a");
+	foreach($results as $port => $resul){
+		$serv1 = getservbyport($port, "tcp");
+		$serv2 = getservbyport($port, "udp");
+
+if($resul){
+
+$write = "
+PORTA                       HOST                  SERVICO
+{$port} esta Aberta         {$ip}           {$serv1}{$serv2}\n";
+
+fwrite($fp, $write . "\n+--------------------------------------------------------------+\n");
+				}
+				if($argv[1] == "-v" or $argv[2] == "-v"){
+					if($resul){
+						echo "[$hora][+] Porta {$port} esta Aberta {$ip} \n";
+						}else{
+							echo "[$hora][-] Porta {$port} esta Fechada {$ip} \n";
+						}
+					}
+				}
+			}
+
+		sleep(2);
+		echo "\n\n[$hora] Portas Encontradas . . .\n";
+		
+		$file = file_get_contents("portscan.txt");
+		echo $file;
+		$limp = fopen("portscan.txt", "w");
+		fwrite($limp, "");
+		$limp1 = fopen("ips_rede.txt", "w");
+		fwrite($limp1, "");
+		fclose($fp);
+	}
 
 if($argv[1] == "-l"){
 	date_default_timezone_set("America/Sao_Paulo");
@@ -184,13 +258,26 @@ Opcoes:
    -t                          Faz scan em todas as portas(65536)
    -p                          Faz scan nas principais portas
    -l                          Lista as principais portas seus protocolos e servicos
+   -g                          Scan de todos os hosts conectados na rede retornando suas portas abertas
+
 
    Ex: 
-      $argv[0] -t 127.0.0.1
-      $argv[0] -t -v 127.0.0.1
-      $argv[0] -p 127.0.0.1
-      $argv[0] -p -v 127.0.0.1
+      Scan com todas as portas:
 
+                       $argv[0] -t 127.0.0.1
+                       $argv[0] -t -v 127.0.0.1
+
+      Scan de portas principais:
+
+                       $argv[0] -p 127.0.0.1
+                       $argv[0] -p -v 127.0.0.1
+
+      Scan de todos os hosts:
+
+                       $argv[0] -g 127.0.0.1
+                       $argv[0] -g -v 127.0.0.1
+
+    OBS:: Nao usar o http:// ou https://
 ";
 }
 
